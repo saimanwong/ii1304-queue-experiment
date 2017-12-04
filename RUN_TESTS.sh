adf0349dd6a67641874ffa59419ea5c9fccbf35f
root <root@bananacake.rywdpukargdezo2zirpwduqfja.fx.internal.cloudapp.net>
2017-12-04 22:53:46 +0000

    Fixed bug when running Docker

--- RUN_TESTS.sh
+++ RUN_TESTS.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 echo "CORRECTNESS TEST STARTING..."
 sleep 5
@@ -6,13 +6,14 @@ sleep 5
 sleep 5
 echo "CORRECTNESS TEST FINISHED"
 
-echo "RUNNING TIME TEST STARTING..."
-sleep 5
-./test_running_time.sh data_running_time 50
-echo "RUNNING TIME TEST FINISHED..."
 
 echo "VALGRIND TEST STARTING..."
 sleep 5
 ./test_valgrind.sh data_valgrind
 sleep 5
 echo "VALGRIND TEST FINISHED..."
+
+echo "RUNNING TIME TEST STARTING (this test may take a while to finish)..."
+sleep 5
+./test_running_time.sh data_running_time 50
+echo "RUNNING TIME TEST FINISHED..."
