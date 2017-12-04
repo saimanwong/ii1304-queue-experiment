cff536fa066ad1261a8177c9de74353573d26813
Sai Man Wong <saimanwong@bananacake.lan>
2017-12-04 23:29:54 +0100

    Modified report

--- run-correctness-test.sh
+++ run-correctness-test.sh
@@ -4,7 +4,3 @@ make
 ./obj/doubly_linked_list_test_correctness src/test_correctness.input src/test_correctness.output
 ./obj/doubly_linked_list_avg_test_correctness src/test_correctness.input src/test_correctness.output
 ./obj/array_priority_test_correctness src/test_correctness.input src/test_correctness.output
-./obj/singly_linked_list 100
-./obj/doubly_linked_list 100
-./obj/doubly_linked_list_avg 100
-./obj/array_priority 100
