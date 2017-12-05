ecd2d8ca6da6729c5b17c3a5a0211a95e22c438b
Sai Man Wong <saimanwong@bananacake.lan>
2017-12-05 01:31:39 +0100

    Added optional code listing

--- test_correctness.sh
+++ test_correctness.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TOTAL_ELEMENTS=$1
 
-python src/generate-correctness-in-out-file.py $TOTAL_ELEMENTS src
+python src/generate_correctness_in_out_file.py $TOTAL_ELEMENTS src
 ./obj/singly_linked_list_test_correctness src/test_correctness.input src/test_correctness.output
 ./obj/doubly_linked_list_test_correctness src/test_correctness.input src/test_correctness.output
 ./obj/doubly_linked_list_avg_test_correctness src/test_correctness.input src/test_correctness.output
